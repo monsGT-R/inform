70eb3783fbf86989d2ce7af448f2f13f519e4458
Дмитрий <monsgt-r@yandex.ru>
2013-11-11 00:10:56 +0800

    Create Лаб№4 A5.cpp

---
+++ Лаб№4 A5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	char a = 'a', // Иницилизируем a = 'a' как начальная буква.
+		 b = 'a'; // и b = 'a' как начальная буква.
+	bool isOrder = true;// Предполагаем что порядок верный.
+	cout << "Введите буквы: ";
+	while (a != '0') // Цикл выполняестя пока не введем 0
+	{
+		if (a < b) {
+			isOrder = false; // Порядок не верный.
+		}
+		b = a;
+		cin >> a; // Обновляем a.
+	}
+	if (isOrder) { // Если порядок верный.
+		cout << "Буквы расположены по порядку." << endl;
+	} else {
+		cout << "Буквы расположены НЕ по порядку." << endl;
+	}
+}
