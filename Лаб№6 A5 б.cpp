83d0d843e015760032766a0f0563634ea81b30fd
Дмитрий <monsgt-r@yandex.ru>
2013-12-04 22:55:01 +0800

    Update Лаб№6 A5 б.cpp

--- Лаб№6 A5 б.cpp
+++ Лаб№6 A5 б.cpp
@@ -26,7 +26,7 @@ void main()
 	cout << "Седловые индексы точек: ";
 	for (int i = 0; i < length; i++) {
 		for (int j = 0; j < length; j++) {
-			// Ищем в каждой строке минимальную и максимальную ячейку и сохраняем индексы.
+			
 			min = max = true;
 			for (int j1 = 0; j1 < length; j1++) {
 				if (matr[i][j1] < matr[i][j]) min = false;
