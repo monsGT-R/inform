f1c68eadf020a871f40aa4ec3c897d6b313bbf3d
Дмитрий <monsgt-r@yandex.ru>
2013-12-05 00:24:59 +0800

    Update Лаб№6 A5 б.cpp

--- Лаб№6 A5 б.cpp
+++ Лаб№6 A5 б.cpp
@@ -22,26 +22,29 @@ void main()
 	//---------------------------------------------
 
 	//---[Вывод седловых индексов матрицы]---------
-	bool min, max;
 	cout << "Седловые индексы точек: ";
 	for (int i = 0; i < length; i++) {
 		for (int j = 0; j < length; j++) {
-			
-			min = max = true;
+			// Проверяем каждый элемент!
+			bool min = true, max = true;
 			for (int j1 = 0; j1 < length; j1++) {
+				// Проверяем, этот элемент максимальный или минимальный или нито и ни другое в СТРОКЕ.
 				if (matr[i][j1] < matr[i][j]) min = false;
 				if (matr[i][j1] > matr[i][j]) max = false;
 			}
 			if (min) {
+				// Если этот элемент минимальный в строке, то проверяем на максимальность в столбце.
 				max = true;
 				for (int i1 = 0; i1 < length; i1++)
 					if (matr[i1][j] > matr[i][j]) max = false;
 			}
-			if (max) {
+			else if (max) {
+				// Если этот элемент максимальный в строке, то проверяем на минимальность в столбце.
 				min = true;
 				for (int i1 = 0; i1 < length; i1++)
 				if (matr[i1][j] < matr[i][j]) min = false;
 			}
+			// Если и минимальный и максимальный TRUE, то выводим индексы элемента
 			if (min && max) cout << "(" << i << ", " << j << ") ";
 		}
 	}
